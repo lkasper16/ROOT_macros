12c463380bf9a89cb304c2bd92ab639623f96219
lkasper16 <lauren.n.kasper@vanderbilt.edu>
2023-03-08 17:13:16 -0600

    working TChain merged plots

--- multiFileAnalyze.C
+++ multiFileAnalyze.C
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
 #include <string>
 #include <TTree.h>
@@ -14,8 +15,8 @@
 #include <TList.h>
 #include <TPaveStats.h>
 #include <TChain.h>
-#include <TH1F.h>
-#include <TH2F.h>
+#include <TH1D.h>
+#include <TH2D.h>
 #include <TF1.h>
 #include <TGraph.h>
 #include <TGraphErrors.h>
@@ -27,265 +28,254 @@
 #include <TProfile.h>
 #include <TTreeReader.h>
 #include <TTreeReaderValue.h>
+#include <TTreeReaderArray.h>
 
 using namespace std;
 //using namespace ROOT;
 
+bool CheckValue(ROOT::Internal::TTreeReaderValueBase& value) {
+   if (value.GetSetupStatus() < 0) {
+      std::cerr << "Error " << value.GetSetupStatus()
+                << "setting up reader for " << value.GetBranchName() << '\n';
+      return false;
+   }
+   return true;
+}
+
 void multiFileAnalyze(){
   
-  TStopwatch timer;
-  timer.Start();
-  
-  //A chain is a collection of files containg TTree objects
-  //Use TChain for multiple files - All constructors are equivalent
-  TChain ch("fdctest");
-  //ArCO2 files
-  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002057.evio.disproot");
+  	TStopwatch timer;
+  	timer.Start();
+  	//Use TChain for multiple files - All constructors are equivalent
+  	TChain ch("fdctest");
+	
+	//// ArCO2 files ////
+//  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002057.evio.disproot");
 //  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002024.evio.disproot");
-//  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002039.evio.disproot");
+    ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002039_NewMapping.evio.disproot");
 //  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002057.evio.disproot");
 //  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002062.evio.disproot");
-  
-  //XeCO2 files
-  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002029.evio.disproot");
-  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002031.evio.disproot");
-  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002037.evio.disproot");
-  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002038.evio.disproot");
-  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002053.evio.disproot");
-  
-  //char buffer[21];
-  int nTrees = ch.GetNtrees();
-  //sprintf(buffer, "%d", nTrees);
-  int nEnts = ch.GetEntries();
-  cout << "Total # of trees: " << nTrees <<  endl;
-  cout << "Total # of entries: " << nEnts <<  endl;
-  
-  int EVENT;
-  int RunNumber;
-  int trig=0;
-  float dped=0.;
-  int dchmax=0;
-  float dsig=0;
-  int dwid=0;
-  int dsmax=0;
-  float wmax=0.;
-  float wped=0.;
-  int wchmax=0;
-  float wcent=0;
-  float wfit=0;
-  float wmcent=0;
-  float wmfit=0;
-  float wsig=0;
-  int wwid=0;
-  int wsmax=0;
-  float w2ped=0.;
-  int w2chmax=0;
-  float w2sig=0;
-  int w2wid=0;
-  int w2smax=0;
-  float dcharge=0.;
-  float wcharge=0.;
-  float w2charge=0.;
-  float dmcharge=0.;
-  float wmcharge=0.;
-  float w2mcharge=0.;
-  int dsize=0;
-  int wsize=0;
-  int w2size=0;
-  int wnhit;
-  float wthit[1000];
-  float wahit[1000];
-  float wmhit[1000];
-  int wchit[1000];
-//  int unhit;
-//  float uthit[1000];
-//  float uahit[1000];
-  int w2nhit;
-  float w2thit[1000];
-  float w2ahit[1000];
-  float w2mhit[1000];
-  int w2chit[1000];
-  int dnhit;
-  float dthit[1000];
-  float dahit[1000];
-  float dmhit[1000];
-  int dchit[1000];
+// 	ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002069.evio.disproot");
 
-  ch.SetBranchAddress("ev", &EVENT);
-  ch.SetBranchAddress( "runNumber", &RunNumber);
-  ch.SetBranchAddress("max_w", &wmax);
-  ch.SetBranchAddress("ped_d", &dped);
-  ch.SetBranchAddress("ped_w", &wped);
-  ch.SetBranchAddress("ped_w2", &w2ped); 
-  ch.SetBranchAddress("q_d", &dcharge);
-  ch.SetBranchAddress("q_w", &wcharge);
-  ch.SetBranchAddress("q_w2", &w2charge);
-  ch.SetBranchAddress("qm_d", &dmcharge);
-  ch.SetBranchAddress("qm_w", &wmcharge);
-  ch.SetBranchAddress("qm_w2", &w2mcharge);
-  ch.SetBranchAddress("dsig", &dsig);
-  ch.SetBranchAddress("dwid", &dwid);
-  ch.SetBranchAddress("wcent", &wcent);
-  ch.SetBranchAddress("wfit", &wfit);
-  ch.SetBranchAddress("wmcent", &wmcent);
-  ch.SetBranchAddress("wmfit", &wmfit);
-  ch.SetBranchAddress("wsig", &wsig);
-  ch.SetBranchAddress("wwid", &wwid);
-  ch.SetBranchAddress("w2sig", &w2sig);
-  ch.SetBranchAddress("w2wid", &w2wid);
-  ch.SetBranchAddress("t_d", &dsmax);
-  ch.SetBranchAddress("t_w", &wsmax);
-  ch.SetBranchAddress("t_w2", &w2smax);
-  ch.SetBranchAddress("dch", &dchmax);
-  ch.SetBranchAddress("wch", &wchmax);
-  ch.SetBranchAddress("w2ch", &w2chmax);
-  ch.SetBranchAddress("dsz", &dsize);
-  ch.SetBranchAddress("wsz", &wsize);
-  ch.SetBranchAddress("w2sz", &w2size);
-  ch.SetBranchAddress("trig", &trig);
-  ch.SetBranchAddress("wnhit", &wnhit);
-  ch.SetBranchAddress("wthit", &wthit);
-  ch.SetBranchAddress("wahit", &wahit);
-  ch.SetBranchAddress("wmhit", &wmhit);
-  ch.SetBranchAddress("wchit", &wchit);
-  ch.SetBranchAddress("w2nhit", &w2nhit);
-  ch.SetBranchAddress("w2thit", &w2thit);
-  ch.SetBranchAddress("w2ahit", &w2ahit);
-  ch.SetBranchAddress("w2mhit", &w2mhit);
-  ch.SetBranchAddress("w2chit", &w2chit);
-//  ch.SetBranchAddress("unhit", &unhit);
-//  ch.SetBranchAddress("uthit", &uthit);
-//  ch.SetBranchAddress("uahit", &uahit);
-  ch.SetBranchAddress("dnhit", &dnhit);
-  ch.SetBranchAddress("dthit", &dthit);
-  ch.SetBranchAddress("dahit", &dahit);
-  ch.SetBranchAddress("dmhit", &dmhit);
-  ch.SetBranchAddress("dchit", &dchit);
-  
-//  TFile* outputFile = TFile::Open("combined_fdctest.root","RECREATE");
-//  outputFile->mkdir("~/TRDPrototype/")->cd();
-//  ch.Merge("combined_fdctest.root"); //Merge all entries in the chain to a new tree in this new file
-//  ch.MakeClass("treeAnalyzer");
-//  ch.ls();
-  
-  TH1F *h1_qm = new TH1F("h1_qm","Max Amplitude Distribution", 4300, -1, 4299);
-  
-  
-  // Each chain element has a tree named 'fdctest' and is titled by the file name the tree came from
-  TObjArray *fileElements=ch.GetListOfFiles();
-  TIter next(fileElements);
-  TChainElement *chEl=0;
-  while (( chEl=(TChainElement*)next() )) {
+	//// XeCO2 files ////
+//  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002029.evio.disproot");
+//  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002031.evio.disproot");
+    ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002037.evio.disproot");
+//  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002038.evio.disproot");
+//  ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002053.evio.disproot");
+// 	ch.Add("~/TRDPrototype/disprootFiles/hd_rawdata_002068.evio.disproot");
   	
-  	 TFile f(chEl->GetTitle());
-/*	 TTreeReader reader("fdctest", &f);
-	 if (f->GetName().conatins("2057")) {
-		TTreeReaderValue<float> dmcharge(reader, "qm_d");
-        TTreeReaderValue<float> w2mcharge(reader, "qm_w2");
-		
-		bool firstEntry = true;
-		while (reader.Next()) {
-			if (firstEntry) {
-         		// Check that branches exist and their types match our expectation.
-         		if (!CheckValue(dmcharge)) return false;
-         		if (!CheckValue(w2mcharge)) return false;
-         		firstEntry = false;
-      		}
-			
-			// Access the vector of values
-      		if (!dmcharge.IsEmpty()&&!w2mcharge.IsEmpty()) {
-         		// Access a float value - need to dereference as TTreeReaderValue
-         		// behaves like an iterator
-    			h1_qm->Fill(*dmcharge)
-				h1_qm->Fill(*w2mcharge,"","same")
-			}
-		
-	 	} 
-*/
-	 int nTreeEnts(chEl->GetEntries());
- 	 cout<<"# Entries in "<<chEl->GetTitle()<<": "<<nTreeEnts<<endl;
-  	 f.Draw("qm_d>>h1_qm");
-	 f.Draw("qm_w2>>h1_qm");
-     
- 	 
-   }
-  
-  
-  //// GEM Histos ////
-  TH2D *w2AvsT = new TH2D("w2AvsT","GEM Amplitude vs Time",300,-0.5,299.5,240,-0.5,239.5);
-//  TH2D *w2AvsTnorm = new TH2D("w2AvsTnorm","GEM Amplitude vs Time norm",300,-0.5,299.5,240,-0.5,239.5);
-  TH2D *w2CvsT = new TH2D("w2CvsT","GEM Width (transverse) vs Time",300,-0.5,299.5,240,-0.5,239.5);
-//  TH2D *w2CvsL = new TH2D("w2CvsL","GEM Width (longitudinal) vs Time",300,-0.5,299.5,240,-0.5,239.5);
-  TH2D *w2AvsB = new TH2D("w2AvsB","GEM Amplitude vs Time (background)",300,-0.5,299.5,240,-0.5,239.5);
-  TH2D *w2CvsB = new TH2D("w2CvsB","GEM Counts vs Time (background)",300,-0.5,299.5,240,-0.5,239.5);
-  TH2D *w2YvsB = new TH2D("w2YvsB","GEM X vs Y (background)",240,-0.5,239.5,240,-0.5,239.5);
-  TH2D *w2TYvsB = new TH2D("w2TYvsB","GEM TY vs Time (background)",300,-0.5,299.5,240,-0.5,239.5);
-  TH2D *w2AvsD = new TH2D("w2AvsD","GEM Amplitude vs Distance",1000,0.,35.,240,-0.5,239.5);
-//  w2AvsTnorm->Sumw2();
-  w2AvsT->Sumw2();
-  w2CvsT->Sumw2();
-//  w2CvsL->Sumw2();
-  w2AvsB->Sumw2();
-  w2CvsB->Sumw2();
-  w2YvsB->Sumw2();
-  w2TYvsB->Sumw2();
-  w2AvsD->Sumw2();
+  	int nTrees = ch.GetNtrees();
+  	int nEnts = ch.GetEntries();
+  	cout << "Total # of trees: " << nTrees <<  endl;
+  	cout << "Total # of entries: " << nEnts <<  endl;
+	
+	//TFile* outputFile = TFile::Open("combined_fdctest.root","RECREATE");
+  	//outputFile->mkdir("~/TRDPrototype/")->cd();
+  	//ch.Merge(outputFile, 0); //Merge all entries in the chain to a new tree in this new file
+	//ch.MakeClass("treeAnalyzer");
+	//ch.ls();
+  	
+	//// Define Histograms ////
+  	//TH1D *h1_qm = new TH1D("h1_qm","Max Amplitude Distribution", 4300, -1, 4299);
+  	TH2D *w2AvsT = new TH2D("w2AvsT","GEM Amplitude vs Time",300,-0.5,299.5,240,-0.5,239.5);
+  	w2AvsT->Sumw2();
+  	TH2D *dAvsT = new TH2D("dAvsT","MMG Amplitude vs Time",300,-0.5,299.5,200,-0.5,199.5);
+  	dAvsT->Sumw2();
+ 	
+	TH1D *qm_d_XeCO2 = new TH1D("qm_d_XeCO2","Max Amplitude Distribution - Half Fleece Rad",1000,0.,4500);
+	TH1D *qm_w2_XeCO2 = new TH1D("qm_w2_XeCO2","Max Amplitude Distribution - Half Fleece Rad",1000,0.,5000);
+	TH1D *qm_d_ArCO2 = new TH1D("qm_d_ArCO2","Max Amplitude Distribution - Half Fleece Rad",1000,0.,4500);
+    TH1D *qm_w2_ArCO2 = new TH1D("qm_w2_ArCO2","Max Amplitude Distribution - Half Fleece Rad",1000,0.,4500);
 
+	//// Define TREE variables ////
+	int EVENT;
+	int RunNumber;
+	int samples;
+	int isamp=0;
+	int trig=0;
+	
+	float dped=0.;
+	int dchmax=0;
+	float dsig=0;
+	int dwid=0;
+	int dsmax=0;
+	//float dAmpmax=0.;
+	float w2ped=0.;
+	int w2chmax=0;
+	float w2sig=0;
+	int w2wid=0;
+	int w2smax=0;
+	//float w2Ampmax=0.;
+	float dcharge=0.;
+	float w2charge=0.;
+	float dmcharge=0.;
+	float w2mcharge=0.;
+	int dsize=0;
+	int w2size=0;
+	int w2nhit;
+	float w2thit[1000];
+	float w2ahit[1000];
+	float w2mhit[1000];
+	int w2chit[1000];
+	int dnhit;
+	float dthit[1000];
+	float dahit[1000];
+	float dmhit[1000];
+	int dchit[1000];
+	
+	/*
+  	TTreeReader myReader("fdctest", outputFile); // name of tree and file/chain (&f/&ch)
+  	TTreeReaderValue<Float_t> dmcharge(myReader, "qm_d");
+  	TTreeReaderValue<Float_t> w2mcharge(myReader, "qm_w2");
+  	TTreeReaderValue<Int_t> EVENT(myReader, "ev");
+	TTreeReaderValue<Int_t> RunNumber(myReader, "runNumber");
+	TTreeReaderValue<Int_t> dchmax(myReader, "dch");
+	TTreeReaderValue<Int_t> w2chmax(myReader, "w2ch");
+	TTreeReaderValue<Float_t> dsmax(myReader, "t_d");
+	TTreeReaderValue<Float_t> w2smax(myReader, "t_w2");
+	TTreeReaderValue<Float_t> dped(myReader, "ped_d");
+	TTreeReaderValue<Float_t> dsig(myReader, "dsig");
+	TTreeReaderValue<Int_t> dwid(myReader, "dwid");
+	TTreeReaderValue<Float_t> w2ped(myReader, "ped_w2");
+	TTreeReaderValue<Float_t> w2sig(myReader, "w2sig");
+	TTreeReaderValue<Int_t> w2wid(myReader, "w2wid");
+	TTreeReaderValue<Float_t> dcharge(myReader, "q_d");
+	TTreeReaderValue<Float_t> w2charge(myReader, "q_w2");
+	TTreeReaderValue<Int_t> dsize(myReader, "dsz");
+	TTreeReaderValue<Int_t> w2size(myReader, "w2sz");
+	TTreeReaderValue<Int_t> w2nhit(myReader, "w2nhit");
+	TTreeReaderValue<Int_t> dnhit(myReader, "dnhit");
+	TTreeReaderArray<Float_t> w2thit(myReader, "w2thit");
+	TTreeReaderArray<Float_t> w2ahit(myReader, "w2ahit");
+	TTreeReaderArray<Float_t> w2mhit(myReader, "w2mhit");
+	TTreeReaderArray<Float_t> w2chit(myReader, "w2chit");
+	TTreeReaderArray<Float_t> dthit(myReader, "dthit");
+	TTreeReaderArray<Float_t> dahit(myReader, "dahit");
+	TTreeReaderArray<Float_t> dmhit(myReader, "dmhit");
+	TTreeReaderArray<Float_t> dchit(myReader, "dchit");
+*/	
+	ch.SetBranchAddress("ev", &EVENT);
+	ch.SetBranchAddress("runNumber", &RunNumber);
+	ch.SetBranchAddress("qm_d", &dmcharge);
+	ch.SetBranchAddress("qm_w2", &w2mcharge);
+	ch.SetBranchAddress("dch", &dchmax);
+	ch.SetBranchAddress("w2ch", &w2chmax);
+	ch.SetBranchAddress("t_d", &dsmax);
+	ch.SetBranchAddress("t_w2", &w2smax);
+	ch.SetBranchAddress("ped_d", &dped);
+	ch.SetBranchAddress("ped_w2", &w2ped);
+	ch.SetBranchAddress("q_d", &dcharge);
+	ch.SetBranchAddress("q_w2", &w2charge);
+	ch.SetBranchAddress("dsig", &dsig);
+	ch.SetBranchAddress("dwid", &dwid);
+	ch.SetBranchAddress("w2sig", &w2sig);
+	ch.SetBranchAddress("w2wid", &w2wid);
+	ch.SetBranchAddress("dsz", &dsize);
+	ch.SetBranchAddress("w2sz", &w2size);
+	//ch.SetBranchAddress("trig", &trig);
+	ch.SetBranchAddress("w2nhit", &w2nhit);
+	ch.SetBranchAddress("w2thit", &w2thit);
+	ch.SetBranchAddress("w2ahit", &w2ahit);
+	ch.SetBranchAddress("w2mhit", &w2mhit);
+	ch.SetBranchAddress("w2chit", &w2chit);
+	ch.SetBranchAddress("dnhit", &dnhit);
+	ch.SetBranchAddress("dthit", &dthit);
+	ch.SetBranchAddress("dahit", &dahit);
+	ch.SetBranchAddress("dmhit", &dmhit);
+	ch.SetBranchAddress("dchit", &dchit);
+	
+	//// Reading Tree ////
+ 	for(int i = 0; i < nEnts; i++){
+		
+		ch.GetEntry(i);
+/*		//// Loop through all events ////
+		bool firstEntry = true;
+		while (myReader.Next()) {
+			if (firstEntry) {
+        		// Check that branches exist and their types match our expectation.
+            	if (!CheckValue(EVENT)) return false;
+            	if (!CheckValue(RunNumber)) return false;
+            	firstEntry = false;
+        	}
+*/
+		if (RunNumber == 2037) {
+				
+			if(dmcharge > 0){
+				qm_d_XeCO2->Fill(dmcharge);  
+			}
+			if(w2mcharge > 0) {
+			    qm_w2_XeCO2->Fill(w2mcharge);  
+			}
+		}	
+		if (RunNumber == 2039) {
+			
+			if(dmcharge > 0){
+                qm_d_ArCO2->Fill(dmcharge);
+            }	
+			if(w2mcharge > 0){
+                qm_w2_ArCO2->Fill(w2mcharge);
+            }
+		}
+ 	} // End loop over TChain entries
+	
+	TCanvas *c1 = new TCanvas("c1","c1", 1200, 800);
+	gStyle->SetOptStat(00000);
+	c1->cd();
+	gPad->SetLogy();
+	
+	qm_d_XeCO2->SetLineColor(2);
+	qm_d_XeCO2->GetXaxis()->SetTitle("Maximum ADC values");
+	//qm_d_XeCO2->GetXaxis()->CenterTitle();
+	//qm_d_XeCO2->GetXaxis()->SetTitleSize(0.2);
+	qm_d_XeCO2->GetYaxis()->SetTitle("Counts");
+	qm_d_XeCO2->GetYaxis()->SetRangeUser(0.1, 10000);
+	qm_d_XeCO2->Draw();
+	
+	qm_w2_XeCO2->SetLineColor(4);
+    qm_w2_XeCO2->Draw("same");
+	
+	qm_d_ArCO2->SetLineColor(208);
+	qm_d_ArCO2->SetLineStyle(3);
+	qm_d_ArCO2->SetLineWidth(3);
+    qm_d_ArCO2->Draw("same");
+	
+	qm_w2_ArCO2->SetLineColor(64);
+    qm_w2_ArCO2->SetLineStyle(3);
+	qm_w2_ArCO2->SetLineWidth(3);
+    qm_w2_ArCO2->Draw("same");
+	
+	TLegend *l1 = new TLegend(0.5, 0.8, 0.9, 0.9);
+	l1->AddEntry(qm_w2_XeCO2, "GEM XeCO2", "L");
+	l1->AddEntry(qm_d_XeCO2, "MMG XeCO2", "L");
+	l1->AddEntry(qm_w2_ArCO2, "GEM ArCO2", "L");
+	l1->AddEntry(qm_d_ArCO2, "MMG ArCO2", "L");
+	l1->Draw();
+
+	timer.Stop();
+    timer.Print();
+
+}
+
+
+
+  //// GEM Histos ////
+/*  TH2D *w2AvsT = new TH2D("w2AvsT","GEM Amplitude vs Time",300,-0.5,299.5,240,-0.5,239.5);
+  w2AvsT->Sumw2();
   TAxis *xaxis = (TAxis*)w2AvsT->GetXaxis();
   xaxis->SetTitle("Time (*8 ns)");
   TAxis *yaxis = (TAxis*)w2AvsT->GetYaxis();
   yaxis->SetTitle("Channel");
-  xaxis = (TAxis*)w2AvsB->GetXaxis();
-  xaxis->SetTitle("Time (*8 ns)");
-  yaxis = (TAxis*)w2AvsB->GetYaxis();
-  yaxis->SetTitle("Channel");
-  xaxis = (TAxis*)w2CvsB->GetXaxis();
-  xaxis->SetTitle("Time (*8 ns)");
-  yaxis = (TAxis*)w2CvsB->GetYaxis();
-  yaxis->SetTitle("Channel");
   
   //// MMG Histos ////
   TH2D *dAvsT = new TH2D("dAvsT","MMG Amplitude vs Time",300,-0.5,299.5,200,-0.5,199.5);
-//  TH2D *dAvsTnorm = new TH2D("dAvsTnorm","MMG Amplitude vs Time norm",300,-0.5,299.5,200,-0.5,199.5);
-  TH2D *dCvsT = new TH2D("dCvsT","MMG Width (transverse) vs Time",300,-0.5,299.5,200,-0.5,199.5);
-//  TH2D *dCvsL = new TH2D("dCvsL","MMG Width (longitudinal) vs Time",300,-0.5,299.5,200,-0.5,199.5);
-  TH2D *dAvsB = new TH2D("dAvsB","MMG Amplitude vs Time (background)",300,-0.5,299.5,200,-0.5,199.5);
-  TH2D *dCvsB = new TH2D("dCvsB","MMG Counts vs Time (background)",300,-0.5,299.5,200,-0.5,199.5);
-  TH2D *dYvsB = new TH2D("dYvsB","MMG X vs Y (background)",200,-0.5,199.5,200,-0.5,199.5);
-  TH2D *dTYvsB = new TH2D("dTYvsB","MMG TY vs Time (background)",1000,-0.5,999.5,200,-0.5,199.5);
-  TH2D *dAvsD = new TH2D("dAvsD","MMG Amplitude vs Distance",1000,0.,35.,200,-0.5,199.5);
-//  dAvsTnorm->Sumw2();
   dAvsT->Sumw2();
-  dCvsT->Sumw2();
-//  dCvsL->Sumw2();
-  dAvsB->Sumw2();
-  dCvsB->Sumw2();
-  dYvsB->Sumw2();
-  dTYvsB->Sumw2();
-  dAvsD->Sumw2();
-  
   xaxis = (TAxis*)dAvsT->GetXaxis();
   xaxis->SetTitle("Time (*8 ns)");
   yaxis = (TAxis*)dAvsT->GetYaxis();
   yaxis->SetTitle("Channel");
-  xaxis = (TAxis*)dAvsB->GetXaxis();
-  xaxis->SetTitle("Time (*8 ns)");
-  yaxis = (TAxis*)dAvsB->GetYaxis();
-  yaxis->SetTitle("Channel");
-  xaxis = (TAxis*)dCvsT->GetXaxis();
-  xaxis->SetTitle("Time (*8 ns)");
-  yaxis = (TAxis*)dCvsT->GetYaxis();
-  yaxis->SetTitle("Channel");
-  xaxis = (TAxis*)dCvsB->GetXaxis();
-  xaxis->SetTitle("Time (*8 ns)");
-  yaxis = (TAxis*)dCvsB->GetYaxis();
-  yaxis->SetTitle("Channel");
-  
-  //// 1D Comparisons ////
- 
   
   timer.Stop();
   timer.Print(); 
-  
-}
+*/
 
