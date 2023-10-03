96492d66c1c53b5500c1efdb0704ff75c6aacb52
lkasper16 <lauren.n.kasper@vanderbilt.edu>
2023-10-03 13:59:59 -0500

    Foil rad comparison

--- projection2D.C
+++ projection2D.C
@@ -28,45 +28,56 @@
 void projection2D(){
 	
 	//TString inputDir = "../fermiDataAnalysis/cherDownUpNoBoxOutput/RootOutput";
-	TString rootFiles[] = {"Run003288_Output.root"};
+	TString rootFiles[] = {"Run003202_Output.root","Run003203_Output.root"};
 	TList *mmg1List = new TList;
 	TList *gemList = new TList;
 	TList *mmg2List = new TList;
-	//TString legendList[] = {"Electrons","Pions"};
+	TList *urwList = new TList;
+	TString legendList[] = {"Foil Radiator (Electrons)","Foil Radiator (Pions)","No Radiator (Electrons)","No Radiator (Pions)"};
 	TLegend *l1 = new TLegend(0.75, 0.65, 0.9, 0.9);
 	TLegend *l2 = new TLegend(0.75, 0.65, 0.9, 0.9);
 	TLegend *l3 = new TLegend(0.75, 0.65, 0.9, 0.9);
+	TLegend *l4 = new TLegend(0.75, 0.65, 0.9, 0.9);
+	l1->SetNColumns(2);
+	l2->SetNColumns(2);
+	l3->SetNColumns(2);
+	l4->SetNColumns(2);
 	
 	for (int i=0; i<sizeof(rootFiles)/sizeof(rootFiles[0]); i++) {
 		const TString& rootFile = rootFiles[i];
 		TFile *file = TFile::Open(rootFile, "READ");
 		TList *HistDQM = (TList *)file->Get("HistDQM");
 		TIter next(HistDQM);
-		cout<<"Iterate through HistDQM"<<endl;
 		
 		while (TH1 *readObject = dynamic_cast<TH1*>(next())) {
 			TString histName = readObject->GetName();
-			cout<<histName<<endl;
+			//cout<<histName<<endl;
 			if (histName == "mmg1_f125_el_amp2d") { //--MMG1 Electrons
 				TH1D *Xprojection = ((TH2*)readObject)->ProjectionX();
 				TH1D *XprojectionClone = new TH1D(*Xprojection);
 				XprojectionClone->SetDirectory(0);
 				XprojectionClone->SetLineColor(2);
 				XprojectionClone->SetLineWidth(2);
+				XprojectionClone->SetMarkerColor(2);
+				XprojectionClone->SetMarkerStyle(8);
+				if (i==1) XprojectionClone->SetMarkerStyle(4);
 				double elScaleFactor = 1./XprojectionClone->GetEntries();
 				XprojectionClone->Scale(elScaleFactor);
 				mmg1List->Add(XprojectionClone);
-				l1->AddEntry(XprojectionClone, "Electrons", "l");
+				if (i==0) l1->AddEntry(XprojectionClone, legendList[i], "lp"); else if (i==1) l1->AddEntry(XprojectionClone, legendList[i+1], "lp");
+				
 			} else if (histName == "mmg1_f125_pi_amp2d") { //--MMG1 Pions
 				TH1D *Xprojection = ((TH2*)readObject)->ProjectionX();
 				TH1D *XprojectionClone = new TH1D(*Xprojection);
           		XprojectionClone->SetDirectory(0);
 				XprojectionClone->SetLineColor(4);
           		XprojectionClone->SetLineWidth(2);
+				XprojectionClone->SetMarkerColor(4);
+				XprojectionClone->SetMarkerStyle(8);
+				if (i==1) XprojectionClone->SetMarkerStyle(4);
 				double piScaleFactor = 1./XprojectionClone->GetEntries();
           		XprojectionClone->Scale(piScaleFactor);
-         		l1->AddEntry(XprojectionClone, "Pions", "l");
-				cout<<"Clone name: "<<XprojectionClone->GetName()<<endl;
+         		if (i==0) l1->AddEntry(XprojectionClone, legendList[i+1], "lp"); else if (i==1) l1->AddEntry(XprojectionClone, legendList[i+2], "lp");
          		mmg1List->Add(XprojectionClone);
 				
 			} else if (histName == "f125_el_amp2d") { //--GEMTRD Electrons
@@ -75,9 +86,12 @@ void projection2D(){
                 XprojectionClone->SetDirectory(0);
 				XprojectionClone->SetLineColor(2);
                 XprojectionClone->SetLineWidth(2);
+				XprojectionClone->SetMarkerColor(2);
+				XprojectionClone->SetMarkerStyle(8);
+				if (i==1) XprojectionClone->SetMarkerStyle(4);
                 double elScaleFactor = 1./XprojectionClone->GetEntries();
                 XprojectionClone->Scale(elScaleFactor);
-                l2->AddEntry(XprojectionClone, "Electrons", "l");
+                if (i==0) l2->AddEntry(XprojectionClone, legendList[i], "lp"); else if (i==1) l2->AddEntry(XprojectionClone, legendList[i+1], "lp");
                 gemList->Add(XprojectionClone);
 				
             } else if (histName == "f125_pi_amp2d") { //--GEMTRD Pions
@@ -86,20 +100,25 @@ void projection2D(){
                 XprojectionClone->SetDirectory(0);
 				XprojectionClone->SetLineColor(4);
                 XprojectionClone->SetLineWidth(2);
+				XprojectionClone->SetMarkerColor(4);
+				XprojectionClone->SetMarkerStyle(8);
+				if (i==1) XprojectionClone->SetMarkerStyle(4);
                 double piScaleFactor = 1./XprojectionClone->GetEntries();
                 XprojectionClone->Scale(piScaleFactor);
-                l2->AddEntry(XprojectionClone, "Pions", "l");
+                if (i==0) l2->AddEntry(XprojectionClone, legendList[i+1], "lp"); else if (i==1) l2->AddEntry(XprojectionClone, legendList[i+2], "lp");
                 gemList->Add(XprojectionClone);
 				
-            } else if (histName == "mmg2_f125_el_amp2d") { //--MMG2 Electrons
+			} else if (histName == "mmg2_f125_el_amp2d") { //--MMG2 Electrons
                 TH1D *Xprojection = ((TH2*)readObject)->ProjectionX();
                 TH1D *XprojectionClone = new TH1D(*Xprojection);
                 XprojectionClone->SetDirectory(0);
                 XprojectionClone->SetLineColor(2);
                 XprojectionClone->SetLineWidth(2);
+				XprojectionClone->SetMarkerColor(2);
+				XprojectionClone->SetMarkerStyle(8);
                 double elScaleFactor = 1./XprojectionClone->GetEntries();
                 XprojectionClone->Scale(elScaleFactor);
-                l3->AddEntry(XprojectionClone, "Electrons", "l");
+                l3->AddEntry(XprojectionClone, legendList[i], "lp");
                 mmg2List->Add(XprojectionClone);
 
             } else if (histName == "mmg2_f125_pi_amp2d") { //--MMG2 Pions
@@ -108,10 +127,42 @@ void projection2D(){
                 XprojectionClone->SetDirectory(0);
                 XprojectionClone->SetLineColor(4);
                 XprojectionClone->SetLineWidth(2);
+				XprojectionClone->SetMarkerColor(4);
+				XprojectionClone->SetMarkerStyle(8);
                 double piScaleFactor = 1./XprojectionClone->GetEntries();
                 XprojectionClone->Scale(piScaleFactor);
-                l3->AddEntry(XprojectionClone, "Pions", "l");
+                l3->AddEntry(XprojectionClone, legendList[i+1], "lp");
                 mmg2List->Add(XprojectionClone);
+                
+            }
+            else if (histName == "urw_f125_el_amp2d") { //--URW Electrons
+                TH1D *Xprojection = ((TH2*)readObject)->ProjectionX();
+                TH1D *XprojectionClone = new TH1D(*Xprojection);
+                XprojectionClone->SetDirectory(0);
+                XprojectionClone->SetLineColor(2);
+                XprojectionClone->SetLineWidth(2);
+				XprojectionClone->SetMarkerColor(2);
+				XprojectionClone->SetMarkerStyle(8);
+				if (i==1) XprojectionClone->SetMarkerStyle(4);
+                double elScaleFactor = 1./XprojectionClone->GetEntries();
+                XprojectionClone->Scale(elScaleFactor);
+                if (i==0) l4->AddEntry(XprojectionClone, legendList[i], "lp"); else if (i==1) l4->AddEntry(XprojectionClone, legendList[i+1], "lp");
+                urwList->Add(XprojectionClone);
+				
+            } else if (histName == "urw_f125_pi_amp2d") { //--URW Pions
+                TH1D *Xprojection = ((TH2*)readObject)->ProjectionX();
+                TH1D *XprojectionClone = new TH1D(*Xprojection);
+                XprojectionClone->SetDirectory(0);
+                XprojectionClone->SetLineColor(4);
+                XprojectionClone->SetLineWidth(2);
+				XprojectionClone->SetMarkerColor(4);
+				XprojectionClone->SetMarkerStyle(8);
+				if (i==1) XprojectionClone->SetMarkerStyle(4);
+                double piScaleFactor = 1./XprojectionClone->GetEntries();
+                XprojectionClone->Scale(piScaleFactor);
+                if (i==0) l4->AddEntry(XprojectionClone, legendList[i+1], "lp"); else if (i==1) l4->AddEntry(XprojectionClone, legendList[i+2], "lp");
+                urwList->Add(XprojectionClone);
+    			
             }
 		}
 		cout<<"Close root file"<<endl;
@@ -122,6 +173,7 @@ void projection2D(){
 	TCanvas *c1 = new TCanvas("c1","MMG-1", 1200, 800);
 	TCanvas *c2 = new TCanvas("c2","GEMTRD", 1200, 800);
 	TCanvas *c3 = new TCanvas("c3","MMG-2", 1200, 800);
+	TCanvas *c4 = new TCanvas("c4","uRWell-TRD", 1200, 800);
 	gStyle->SetOptStat(0);
 	
 	//TIter next(mmg1List);
@@ -143,11 +195,11 @@ void projection2D(){
     if (firstmmg1Hist) {
         firstmmg1Hist->GetXaxis()->SetTitle("Time Response (8ns)");
         firstmmg1Hist->GetYaxis()->SetTitle("numEntries / Total Hits");
-        firstmmg1Hist->SetTitle("10GeV XeCO2: MMG1-TRD at 4800V/675V");
+        firstmmg1Hist->SetTitle("10GeV XeCO2: MMG1-TRD at 4825V/625V");
     }
 	mmg1List->Draw("same");
     l1->Draw();
-	c1->SaveAs("PNGs/Run_003288_2DProjection_mmg1.png");
+	c1->SaveAs("PNGs/Run_003202_003203_2DProjection_mmg1.png");
 	
     c2->cd();
 	if (!gemList) {
@@ -163,20 +215,20 @@ void projection2D(){
     if (firstgemHist) {
         firstgemHist->GetXaxis()->SetTitle("Time Response (8ns)");
         firstgemHist->GetYaxis()->SetTitle("numEntries / Total Hits");
-        firstgemHist->SetTitle("10GeV XeCO2: GEM-TRD at 6300V/3300V");
+        firstgemHist->SetTitle("10GeV XeCO2: GEM-TRD at 6200V/3200V");
     }
 	gemList->Draw("same");
 	l2->Draw();
-	c2->SaveAs("PNGs/Run_003288_2DProjection_gem.png");
+	c2->SaveAs("PNGs/Run_003202_003203_2DProjection_gem.png");
 	
 	c3->cd();
 	if (!mmg2List) {
         std::cerr<<"mmg2List is NULL"<<std::endl;
-        return;
+        //return;
     }
     if (mmg2List->IsEmpty()) {
         std::cerr<<"mmg2List is empty"<<std::endl;
-        return;
+        //return;
     }
 	
 	TH1 *firstmmg2Hist = (TH1 *)mmg2List->First();
@@ -185,8 +237,28 @@ void projection2D(){
         firstmmg2Hist->GetYaxis()->SetTitle("numEntries / Total Hits");
         firstmmg2Hist->SetTitle("10GeV XeCO2: MMG2-TRD at 3400V/605V");
     }
-    mmg2List->Draw("same");
-    l3->Draw();
-	c3->SaveAs("PNGs/Run_003288_2DProjection_mmg2.png");
+    //mmg2List->Draw("same");
+    //l3->Draw();
+	//c3->SaveAs("PNGs/Run_003288_2DProjection_mmg2.png");
+	
+	c4->cd();
+	if (!urwList) {
+        std::cerr<<"urwList is NULL"<<std::endl;
+        return;
+    }
+    if (urwList->IsEmpty()) {
+        std::cerr<<"urwList is empty"<<std::endl;
+        return;
+    }
+	
+	TH1 *firsturwHist = (TH1 *)urwList->First();
+    if (firsturwHist) {
+        firsturwHist->GetXaxis()->SetTitle("Time Response (8ns)");
+        firsturwHist->GetYaxis()->SetTitle("numEntries / Total Hits");
+        firsturwHist->SetTitle("10GeV XeCO2: uRWell-TRD at 4500V/495V");
+    }
+    urwList->Draw("same");
+    l4->Draw();
+	c4->SaveAs("PNGs/Run_003202_003203_2DProjection_urw.png");
 	
 }
