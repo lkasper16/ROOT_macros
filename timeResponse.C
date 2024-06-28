940b719f6043851f08351d9c323f591566b3bf76
lkasper16 <lauren.n.kasper@vanderbilt.edu>
2024-06-28 14:12:33 -0500

    Drift time comparison, GEMTRD w rad

--- timeResponse.C
+++ timeResponse.C
@@ -9,8 +9,7 @@
 #include <iostream>
 
 void timeResponse() {
-	gStyle->SetOptStat(0);
-	
+/*
 	TFile *file0 = TFile::Open("Run_003203_Output.root");
 	TList *HistDQM = (TList *)file0->Get("HistDQM");
 	TObject *obj0 = HistDQM->FindObject("f125_el_amp2ds");
@@ -25,23 +24,32 @@ void timeResponse() {
     f125_pi_amp2ds->Scale(piScaleFactor);
 	f125_pi_amp2ds->RebinX(8);
 	f125_pi_amp2ds->ProjectionX("No Radiator, pions",40,200);
-/*
+*/
 	TFile *file1 = TFile::Open("Run_003196_Output.root");
-	HistDQM = (TList *)file1->Get("HistDQM");
+	TList *HistDQM = (TList *)file1->Get("HistDQM");
 	TObject *obj2 = HistDQM->FindObject("f125_el_amp2ds");
 	TH2 *f125_el_amp2ds_r = (TH2 *)obj2;
+	double elScaleFactor = 1./f125_el_amp2ds_r->GetEntries();
+    f125_el_amp2ds_r->Scale(elScaleFactor);
 	f125_el_amp2ds_r->RebinX(8);
 	f125_el_amp2ds_r->ProjectionX("24cm Foil Radiator, electrons",40,200);
 	TObject *obj3 = HistDQM->FindObject("f125_pi_amp2ds");
 	TH2 *f125_pi_amp2ds_r = (TH2 *)obj3;
+	double piScaleFactor = 1./f125_pi_amp2ds_r->GetEntries();
+    f125_pi_amp2ds_r->Scale(piScaleFactor);
 	f125_pi_amp2ds_r->RebinX(8);
 	f125_pi_amp2ds_r->ProjectionX("24cm Foil Radiator, pions",40,200);
-*/
-	TH1F *gem_n_e = (TH1F*)file0->Get("No Radiator, electrons");
-	TH1F *gem_n_pi = (TH1F*)file0->Get("No Radiator, pions");
-//	TH1F *gem_rad_e = (TH1F*)file1->Get("24cm Foil Radiator, electrons");
-//	TH1F *gem_rad_pi = (TH1F*)file1->Get("24cm Foil Radiator, pions");
 	
+	TCanvas *c1 = new TCanvas("c1","", 1200, 800);
+	gStyle->SetOptStat(0);
+	c1->cd();
+ 	gPad->SetGridy();
+	
+//	TH1F *gem_n_e = (TH1F*)file0->Get("No Radiator, electrons");
+//	TH1F *gem_n_pi = (TH1F*)file0->Get("No Radiator, pions");
+	TH1F *gem_rad_e = (TH1F*)file1->Get("24cm Foil Radiator, electrons");
+	TH1F *gem_rad_pi = (TH1F*)file1->Get("24cm Foil Radiator, pions");
+/*
 	gem_n_e->SetLineColor(2); //red
 	gem_n_e->SetMarkerStyle(24); //open circle
 	gem_n_e->SetMarkerColor(2);
@@ -49,7 +57,7 @@ void timeResponse() {
 	gem_n_pi->SetLineColor(4); //blue
 	gem_n_pi->SetMarkerStyle(25); //open square
 	gem_n_pi->SetMarkerColor(4);
-/*
+*/
 	gem_rad_e->SetLineColor(2); //red
 	gem_rad_e->SetMarkerStyle(20); //filled circle
 	gem_rad_e->SetMarkerColor(2);
@@ -57,21 +65,21 @@ void timeResponse() {
 	gem_rad_pi->SetLineColor(4); //blue
 	gem_rad_pi->SetMarkerStyle(21); //filled square
 	gem_rad_pi->SetMarkerColor(4);
-*/
 	
 	TLegend *l1 = new TLegend(0.7,0.7,0.9,0.9);
-	l1->AddEntry(gem_n_e,"Electrons, 10 GeV","lp");
-	l1->AddEntry(gem_n_pi,"Pions, 10 GeV","lp");
-//	l1->AddEntry(gem_rad_e,"24cm Foil Radiator, electrons","lp");
-//	l1->AddEntry(gem_rad_pi,"24cm Foil Radiator, pions","lp");
+//	l1->AddEntry(gem_n_e,"Electrons, 10 GeV","lp");
+//	l1->AddEntry(gem_n_pi,"Pions, 10 GeV","lp");
+	l1->AddEntry(gem_rad_e,"Electrons, 10 GeV","lp");
+	l1->AddEntry(gem_rad_pi,"Pions, 10 GeV","lp");
 	
-	gem_n_e->GetYaxis()->SetTitle("ADC Amplitude Response (Counts / numEntries)");
-	gem_n_e->SetTitle("GEM-TRD ADC Response in Time Without Radiator");
-	gem_n_e->Draw("");
-	//gem_rad_pi->Draw("same");
-	gem_n_pi->Draw("same");
-	//gem_rad_e->Draw("same");
+	gem_rad_e->GetYaxis()->SetTitle("ADC Amplitude Response (Counts / numEntries)");
+	gem_rad_e->GetYaxis()->SetNdivisions(520);
+	gem_rad_e->SetTitle("GEM-TRD ADC Response in Time With 24cm Foil Radiator");
+//	gem_n_e->Draw("");
+	gem_rad_e->Draw("");
+//	gem_n_pi->Draw("same");
+	gem_rad_pi->Draw("same");
 	l1->Draw();
 	
-	gem_n_e->SaveAs("gem_drift_noRad.png");
+	gem_rad_e->SaveAs("gem_drift_doubleFoilRad.png");
 }
